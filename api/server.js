ec38e461cca9f7e9eddc4f5aa9877a00ab2afbd5
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-20 10:20:57 -0500

    added root server testing / with status 200.

--- api/server.js
+++ api/server.js
@@ -3,8 +3,12 @@ const express = require("express");
 const projectsRouter = require("../projects/projects-router");
 
 const server = express();
-
 server.use(express.json());
+
 server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Pinbon server is running :)" });
+});
 
 module.exports = server;
